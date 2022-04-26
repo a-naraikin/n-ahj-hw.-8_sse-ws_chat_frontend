bb0a7d46fdbd27bc949ad698e41dddb80a79d27b
Anna Naraikina <66479710+a-naraikin@users.noreply.github.com>
2022-04-26 13:39:17 +0300

    update url

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import modalLogin from './modal/modalLogin';
 
 const board = document.querySelector('.board');
-const URL = 'https://ahjchat.herokuapp.com';
+// const URL = 'https://ahjchat.herokuapp.com';
 // const URL = 'http://localhost:7070';
+const URL = 'https://whispering-plains-26441.herokuapp.com';
 
 modalLogin(board, URL);
